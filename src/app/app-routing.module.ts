6772bd9fbc413bd7e549278c2a552a750eae65ed
spliitzx <fsmitchm@gmail.com>
2020-06-25 12:53:58 +0800

    Add some routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,31 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Components
 import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { BioComponent } from './components/bio/bio.component';
+import { BooksComponent } from './components/books/books.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'books',
+    component: BooksComponent
+  },
+  {
+    path: 'bio',
+    component: BioComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BooksComponent } from './components/books/books.component';
+import { BioComponent } from './components/bio/bio.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    BooksComponent,
+    BioComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
